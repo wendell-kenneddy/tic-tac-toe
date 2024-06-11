1adab0ab6288464a6c7fe6ad643802b6c21f47d1
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-11 15:31:20 -0300

    feat(App): fill the UI with final components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,7 @@
 import { Header } from "./Header";
 import { Grid } from "./Grid";
+import { PlayerTurnDisplay } from "./PlayerTurnDisplay";
+import { ActionBar } from "./ActionBar";
 
 export function App() {
   return (
@@ -7,7 +9,11 @@ export function App() {
       <Header />
 
       <main className="w-[90%] max-w-[500px] my-4 mx-auto bg-neutral-800 rounded-md p-4 flex flex-col items-center gap-4">
+        <PlayerTurnDisplay />
+
         <Grid />
+
+        <ActionBar />
       </main>
     </>
   );
